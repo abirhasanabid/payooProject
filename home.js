d5b4ce9c582f328c62158a69cdd596a767a9a500
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-09-19 14:20:17 +0600

    compleated dom pyoo fms website

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+// cash in 
 document.getElementById('add-money').addEventListener('click', function (e) {
     e.preventDefault();
 
@@ -13,9 +14,17 @@ document.getElementById('add-money').addEventListener('click', function (e) {
     } else {
         alert('Failed to add money!Please try again')
     }
+
+})
+// first add money btn
+document.getElementById('first-add-money-btn').addEventListener('click',function(){
+const addMoneyClickDiv = document.getElementById('add-money-click-div').classList.remove('hidden');
+const cashOutMoneyClickDiv = document.getElementById('cash-out-money-click-div').classList.add('hidden')
+
 })
 
 
+// cash out
 document.querySelector('#cash-out-money-btn').addEventListener('click', function (e) {
     e.preventDefault();
 
@@ -27,9 +36,16 @@ document.querySelector('#cash-out-money-btn').addEventListener('click', function
         document.querySelector('#main-balance').innerText = restAmmount;
         if (restAmmount < 0) {
             const mainBalance = document.querySelector('#main-balance').innerText = 'invalid ammount';
+            const avbleBalance = document.querySelector('#avble-balance').innerHTML = '';
         }
     } else {
         alert('You must fullfill the two input');
     }
 
+})
+
+// first cash out btn
+document.getElementById('first-cast-out-btn').addEventListener('click',function(){
+    const cashOutMoneyClickDiv = document.getElementById('cash-out-money-click-div').classList.remove('hidden');
+    const addMoneyClickDiv = document.getElementById('add-money-click-div').classList.add('hidden');
 })
