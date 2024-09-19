7eccd6cb7b1edd4ebadeb0a0b6818c06b68deb72
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-09-19 08:46:18 +0600

    just a structure of html home page and add link js file

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-document.getElementById('login-btn').addEventListener('click',function(e){
+// document.getElementById('login-btn').addEventListener('click',function(e){
+//     e.preventDefault();
+//     const inputNumber = document.getElementById('mobile-number').value;
+//     const pinInputValue = document.getElementById('pin').value;
+
+//     if(inputNumber==='5'&& pinInputValue==='1234'){
+//         console.log('you are logged in');
+//     }else{
+//         console.log('worng password or number');
+//     }
+// })
+
+
+document.querySelector('#login-btn').addEventListener('click',function(e){
     e.preventDefault();
-    const inputNumber = document.getElementById('mobile-number').value;
-    console.log(inputNumber);
-    const pinInputValue = document.getElementById('pin').value;
-    console.log(pinInputValue);
+
+    const phoneNumber = document.querySelector('#mobile-number').value;
+    const inputPin = document.querySelector('#pin').value;
+
+    if(phoneNumber==='5'&& inputPin ==='1234'){
+        console.log('You are logged in');
+    }else{
+        alert('Wrong number and password');
+    }
+
+    window.location.href = './homePage.html'
 })
